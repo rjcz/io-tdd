ccea841c9c72d2965f7e41c2e0b81de812ecc040
Maria Morozova <7mm.11mm@gmail.com>
2022-01-23 00:19:59 +0700

    test1Red

---
+++ rejestracjaTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+class rejestracjaTest extends \PHPUnit\Framework\TestCase{
+
+    public function testIsEmailExists(){
+        $rejestracja= new App\Rejestracja;
+        $testemail="123@aa";
+        $result=$rejestracja->isEmailExists($testemail);
+        this->assertTrue($result);
+    }
+   
+}
