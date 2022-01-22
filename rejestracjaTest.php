02b1ddecddfb33e106bd84fa2163fa3055685866
Maria Morozova <7mm.11mm@gmail.com>
2022-01-23 05:10:06 +0700

    test jednostkowy

--- rejestracjaTest.php
+++ rejestracjaTest.php
@@ -14,9 +14,45 @@ class rejestracjaTest extends \PHPUnit\Framework\TestCase{
 
     public function testIsEmailValid(){
         $rejestracja= new App\Rejestracja;
+        //valid
+        $testemail="123@aa.com";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertFalse(!$result);
+
+        $testemail="123@aa.b";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertFalse(!$result);
+
+        $testemail="123@AA.COM";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertFalse(!$result);
+        //non valid
         $testemail="123@aa";
         $result=$rejestracja->isEmailValid($testemail);
         $this->assertTrue(!$result);
+
+        $testemail="123śżć@aa.com";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertTrue(!$result);
+
+        $testemail=" 123@aa.com";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertTrue(!$result);
+
+        
+        $testemail="@aa.com";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertTrue(!$result);
+
+        $testemail="1 23@aa.com";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertTrue(!$result);
+
+        $testemail="";
+        $result=$rejestracja->isEmailValid($testemail);
+        $this->assertTrue(!$result);
+
+      
     }
 
    
